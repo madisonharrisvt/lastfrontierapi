fdce94df764924157457b1f67f36ddd3390122b1
Madison <madisonharrisvt@gmail.com>
2018-08-16 19:24:42 -0700

    15 - Madison - adding events

---
+++ LastFrontierApi/Controllers/EventDetailController.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using LastFrontierApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LastFrontierApi.Controllers
+{
+    [Authorize(Policy = "ApiUser", Roles = "Admin")]
+    [Route("api/[controller]")]
+    public class EventDetailController : Controller
+    {
+        private readonly LfContext _context;
+
+        public EventDetailController(LfContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPut]
+        public Event UpdateOrCreateEvent([FromBody] Event lfEvent)
+        {
+            Event eventToUpdate = null;
+
+            if (lfEvent.Id != 0)
+            {
+                eventToUpdate = _context.tblEvent.FirstOrDefault(e => e.Id == eventToUpdate.Id);
+            }
+
+            if (eventToUpdate != null)
+            {
+                _context.Entry(eventToUpdate).CurrentValues.SetValues(lfEvent);
+            }
+            else
+            {
+                _context.Add(lfEvent);
+            }
+
+            _context.SaveChanges();
+
+            return lfEvent;
+        }
+    }
+}
