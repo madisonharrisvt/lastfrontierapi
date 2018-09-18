8e76582b3710d784fe6582166501ce72b53ef6c1
Madison <madisonharrisvt@gmail.com>
2018-09-18 16:21:12 -0700

    62 - Madison: added ability to add users to roles

--- LastFrontierApi/Models/Validations/CredentialsValidator.cs
+++ LastFrontierApi/Models/Validations/CredentialsValidator.cs
@@ -8,7 +8,7 @@ namespace LastFrontierApi.Models.Validations
         {
             RuleFor(vm => vm.UserName).NotEmpty().WithMessage("Username cannot be empty");
             RuleFor(vm => vm.Password).NotEmpty().WithMessage("Password cannot be empty");
-            RuleFor(vm => vm.Password).Length(6, 12).WithMessage("Password must be between 6 and 12 characters");
+            RuleFor(vm => vm.Password).Length(6, 40).WithMessage("Password must be between 6 and 40 characters");
         }
     }
 }
