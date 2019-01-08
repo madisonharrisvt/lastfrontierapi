7ac52ca24532d7ac3bc5c02495ea9842b44bf05b
Madison <madisonharrisvt@gmail.com>
2019-01-07 19:44:25 -0800

    72 - Madison - added player search, xpBought and Cultures in Character Sheet

--- LastFrontierApi/Controllers/CharacterMetadataController.cs
+++ LastFrontierApi/Controllers/CharacterMetadataController.cs
@@ -8,96 +8,100 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace LastFrontierApi.Controllers
 {
-    [Authorize(Policy = "ApiUser", Roles = "Admin, User")]
-    [Route("api/[controller]")]
-    public class CharacterMetadataController : Controller
+  [Authorize(Policy = "ApiUser", Roles = "Admin, User")]
+  [Route("api/[controller]")]
+  public class CharacterMetadataController : Controller
+  {
+    private readonly LfContext _context;
+
+    public CharacterMetadataController(LfContext context)
     {
-        private readonly LfContext _context;
-
-        public CharacterMetadataController(LfContext context)
-        {
-            _context = context;
-        }
-
-        [HttpGet("{id}")]
-        public CharacterMetadata GetMetadata(int id)
-        {
-            var characterMetadata = new CharacterMetadata()
-            {
-                Occupations = _context.tblOccupation.ToArray(),
-                SideGigs = AvailableSideGigs(id),
-                Skills = _context.tblSkill.ToArray(),
-                Species = _context.tblSpecies.ToArray(),
-                Statuses = _context.tblStatus.ToArray(),
-                StressResponses = _context.tblStressResponse.ToArray()
-            };
-
-            return characterMetadata;
-        }
-
-        [HttpPost]
-        public IActionResult GetSideGigMetadata([FromBody] int chosenOccupation)
-        {
-            try
-            {
-                var availableSideGigs = AvailableSideGigs(chosenOccupation);
-
-                return Ok(availableSideGigs);
-            }
-            catch (Exception e)
-            {
-                return StatusCode(500);
-            }
-            
-        }
-
-        private SideGig[] AvailableSideGigs(int chosenOccupation)
-        {
-            var occupations = _context.tblOccupation.ToArray();
-
-            var orderedOccupations = new List<Occupation>()
-            {
-                occupations.FirstOrDefault(x => x.Id == 3), // Soldier
-                occupations.FirstOrDefault(x => x.Id == 1), // Doctor
-                occupations.FirstOrDefault(x => x.Id == 5), // Prospector
-                occupations.FirstOrDefault(x => x.Id == 2), // Socialite
-                occupations.FirstOrDefault(x => x.Id == 4)  // Engineer
-            };
-
-            var orderedOccupationsList = new LinkedList<Occupation>(orderedOccupations);
-
-            var chosenOccupationInList = orderedOccupationsList.FirstOrDefault(o => o.Id == chosenOccupation);
-
-            var currentOccupation = orderedOccupationsList.Find(chosenOccupationInList);
-
-            if (currentOccupation == null)
-            {
-                return null;
-            }
-
-            var previousOccupation = currentOccupation.Previous ?? orderedOccupationsList.Last;
-            var nextOccupation = currentOccupation.Next ?? orderedOccupationsList.First;
-
-            var previousSideGig = new SideGig()
-            {
-                Id = previousOccupation.Value.Id,
-                Name = previousOccupation.Value.Name
-            };
-
-            var nextSideGig = new SideGig()
-            {
-                Id = nextOccupation.Value.Id,
-                Name = nextOccupation.Value.Name
-            };
-
-
-            var availableSideGigs = new SideGig[]
-            {
-                previousSideGig,
-                nextSideGig
-            };
-
-            return availableSideGigs;
-        }
+      _context = context;
     }
+
+    [HttpGet("{id}")]
+    public IActionResult GetMetadata(int id)
+    {
+      try
+      {
+        var characterMetadata = new CharacterMetadata
+        {
+          Occupations = _context.tblOccupation.ToArray(),
+          SideGigs = AvailableSideGigs(id),
+          Skills = _context.tblSkill.ToArray(),
+          Species = _context.tblSpecies.ToArray(),
+          Statuses = _context.tblStatus.ToArray(),
+          StressResponses = _context.tblStressResponse.ToArray(),
+          Cultures = _context.tblCulture.ToArray()
+        };
+
+        return Ok(characterMetadata);
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpPost]
+    public IActionResult GetSideGigMetadata([FromBody] int chosenOccupation)
+    {
+      try
+      {
+        var availableSideGigs = AvailableSideGigs(chosenOccupation);
+
+        return Ok(availableSideGigs);
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    private SideGig[] AvailableSideGigs(int chosenOccupation)
+    {
+      var occupations = _context.tblOccupation.ToArray();
+
+      var orderedOccupations = new List<Occupation>
+      {
+        occupations.FirstOrDefault(x => x.Id == 3), // Soldier
+        occupations.FirstOrDefault(x => x.Id == 1), // Doctor
+        occupations.FirstOrDefault(x => x.Id == 5), // Prospector
+        occupations.FirstOrDefault(x => x.Id == 2), // Socialite
+        occupations.FirstOrDefault(x => x.Id == 4)  // Engineer
+      };
+
+      var orderedOccupationsList = new LinkedList<Occupation>(orderedOccupations);
+
+      var chosenOccupationInList = orderedOccupationsList.FirstOrDefault(o => o.Id == chosenOccupation);
+
+      var currentOccupation = orderedOccupationsList.Find(chosenOccupationInList);
+
+      if (currentOccupation == null) return null;
+
+      var previousOccupation = currentOccupation.Previous ?? orderedOccupationsList.Last;
+      var nextOccupation = currentOccupation.Next ?? orderedOccupationsList.First;
+
+      var previousSideGig = new SideGig
+      {
+        Id = previousOccupation.Value.Id,
+        Name = previousOccupation.Value.Name
+      };
+
+      var nextSideGig = new SideGig
+      {
+        Id = nextOccupation.Value.Id,
+        Name = nextOccupation.Value.Name
+      };
+
+
+      var availableSideGigs = new[]
+      {
+        previousSideGig,
+        nextSideGig
+      };
+
+      return availableSideGigs;
+    }
+  }
 }
--- LastFrontierApi/Models/LfContext.cs
+++ LastFrontierApi/Models/LfContext.cs
@@ -1,39 +1,31 @@
-﻿using System.Collections.Generic;
-using LastFrontierApi.Models.Metadata;
+﻿using LastFrontierApi.Models.Metadata;
 using Microsoft.EntityFrameworkCore;
 
 namespace LastFrontierApi.Models
 {
-    public class LfContext : DbContext
+  public class LfContext : DbContext
+  {
+    public LfContext(DbContextOptions<LfContext> options)
+      : base(options)
     {
-        public LfContext(DbContextOptions<LfContext> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<Character> tblCharacter { get; set; }
-
-        public DbSet<CharacterSkill> tblCharacterSkills { get; set; }
-
-        public DbSet<Occupation> tblOccupation { get; set; }
-        public DbSet<SideGig> tblSideGig { get; set; }
-        public DbSet<Skill> tblSkill { get; set; }
-        public DbSet<CharacterEvent> tblCharacterEvents { get; set; }
-        public DbSet<Species> tblSpecies { get; set; }
-        public DbSet<Status> tblStatus { get; set; }
-        public DbSet<StressResponse> tblStressResponse { get; set; }
-
-        public DbSet<Event> tblEvent { get; set; }
-
-        public DbSet<NpcShift> tblNpcShift { get; set; }
-        public DbSet<PlayerNpcShifts> tblPlayerNpcShifts { get; set; }
-
-        public DbSet<Cart> tblCart { get; set; }
-        public DbSet<CartItem> tblCartItem { get; set; }
-
-        
-        public DbQuery<NpcShiftWithPlayerCount> Sp_GetNpcShiftsWithPlayerCount { get; set; }
-        public DbQuery<PlayerId> Sp_GetPlayersWithoutNpcShiftForActiveEvent { get; set; }
-
     }
+
+    public DbSet<Character> tblCharacter { get; set; }
+    public DbSet<CharacterSkill> tblCharacterSkills { get; set; }
+    public DbSet<Occupation> tblOccupation { get; set; }
+    public DbSet<SideGig> tblSideGig { get; set; }
+    public DbSet<Skill> tblSkill { get; set; }
+    public DbSet<CharacterEvent> tblCharacterEvents { get; set; }
+    public DbSet<Species> tblSpecies { get; set; }
+    public DbSet<Status> tblStatus { get; set; }
+    public DbSet<StressResponse> tblStressResponse { get; set; }
+    public DbSet<Culture> tblCulture { get; set; }
+    public DbSet<Event> tblEvent { get; set; }
+    public DbSet<NpcShift> tblNpcShift { get; set; }
+    public DbSet<PlayerNpcShifts> tblPlayerNpcShifts { get; set; }
+    public DbSet<Cart> tblCart { get; set; }
+    public DbSet<CartItem> tblCartItem { get; set; }
+    public DbQuery<NpcShiftWithPlayerCount> Sp_GetNpcShiftsWithPlayerCount { get; set; }
+    public DbQuery<PlayerId> Sp_GetPlayersWithoutNpcShiftForActiveEvent { get; set; }
+  }
 }
